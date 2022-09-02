528b387a8a739d9cddb5c8ae64972f9d570bf880
niraj0511 <niraj.mutha9@gmail.com>
2022-09-03 00:04:22 +0530

    Initial Commit

---
+++ qeatsbackend/src/main/java/com/crio/qeats/services/RestaurantServiceImpl.java
@@ -0,0 +1,59 @@
+
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.services;
+
+import com.crio.qeats.dto.Restaurant;
+import com.crio.qeats.exchanges.GetRestaurantsRequest;
+import com.crio.qeats.exchanges.GetRestaurantsResponse;
+import com.crio.qeats.repositoryservices.RestaurantRepositoryService;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class RestaurantServiceImpl implements RestaurantService {
+
+  private final Double peakHoursServingRadiusInKms = 3.0;
+  private final Double normalHoursServingRadiusInKms = 5.0;
+  @Autowired
+  private RestaurantRepositoryService restaurantRepositoryService;
+  @Autowired
+  private GetRestaurantsResponse getRestaurantsResponse;
+
+
+  // TODO: CRIO_TASK_MODULE_RESTAURANTSAPI - Implement findAllRestaurantsCloseby.
+  // Check RestaurantService.java file for the interface contract.
+  @Override
+  public GetRestaurantsResponse findAllRestaurantsCloseBy(
+      GetRestaurantsRequest getRestaurantsRequest, LocalTime currentTime) {
+        List<Restaurant> restaurants=null;
+        Integer currentHour = currentTime.getHour();
+        Integer currentMinute = currentTime.getMinute();
+        if((currentHour>=8 && currentHour<10) ||(currentHour==10 && (currentMinute==0)) ||(currentHour==13) ||(currentHour==14 && (currentMinute==0)) ||(currentHour>=19 && currentHour<21)||(currentHour==21 && (currentMinute==0))){
+          restaurants=restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, peakHoursServingRadiusInKms);
+        }
+        else{
+          restaurants=restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, normalHoursServingRadiusInKms);
+        }
+        getRestaurantsResponse.setRestaurants(restaurants);
+     return getRestaurantsResponse;
+  }
+
+
+}
+
